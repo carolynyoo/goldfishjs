2fec2f0504d200ade3d0565f7584bfe6345932bb
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-06-02 20:08:28 -0400

    Complete seed

--- db/testdb.js
+++ db/testdb.js
@@ -7,18 +7,24 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function() {
   if(!exists) {
-    db.run("CREATE TABLE Planck (id INTEGER PRIMARY KEY, diff TEXT, savedat DATE)");
+    db.run("CREATE TABLE Planck (id INTEGER PRIMARY KEY, text_state TEXT, saved_at DATE, author TEXT)");
   }
 
-	var stmt = db.prepare("INSERT INTO Planck(diff, savedat) VALUES (?, ?)");
-	fs.readFile('helloworld.txt', "utf-8", function(err, text) {
-		console.log('readfile from text', text);
-		datetest = Date.now();
-	  	stmt.run(text, datetest);
-		stmt.finalize();
-		db.each("SELECT id, diff, savedat FROM Planck", function(err, row) {
-		    console.log(row.id + ": " + row.diff, "Saved at : " + row.savedat);
-		});
-	});
+  fs.watchFile('helloworld.txt', function (curr, prev) {
+    readFile();
+  })
  
 });
+
+function readFile () {
+	var stmt = db.prepare("INSERT INTO Planck(text_state, saved_at, author) VALUES (?, ?, ?)");
+  fs.readFile('helloworld.txt', "utf-8", function(err, text) {
+    console.log('readfile from text', text);
+    datetest = Date.now();
+    stmt.run(text, datetest, "Randy Wong");
+    stmt.finalize();
+    db.each("SELECT id, text_state, saved_at, author FROM Planck", function(err, row) {
+      console.log(row.id + ": " + row.text_state, "Saved at : " + row.saved_at, "Author: "+row.author);
+    });
+  });
+}
