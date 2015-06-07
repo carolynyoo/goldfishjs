ca64a757503933dab2fdfe6034a1964ee311ff16
darksim735 <darksim735@gmail.com>
2015-06-07 15:56:52 -0400

    Set up factory to get all keyframes but ran into database connection issue with nodewebkit and sequelize.

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,12 @@
 var app = angular.module('gitplayback', ['ui.router']);
 
-app.controller('MainController', function ($scope) {
+
+app.controller('MainController', function ($scope, KeyframeFactory) {
 
 $scope.framesArray = ['nothing'];
 $scope.currentFrame = "no current frame";
+
+$scope.getall = KeyframeFactory.getKeyframe();
 
 });
 
@@ -12,6 +15,26 @@ app.factory('KeyframeFactory', function ($http) {
 		getKeyframe: function(){
 			// sequelize query goes here
 			console.log("got keyframe?");
+
+
+// Keyframe
+//     .findAll({})
+//     .then(function() {
+      
+//     })
+//     .then(function() {
+//       console.log("Fetched all keyframes succesfully"); 
+//     })
+//     .catch(function(err) {
+//       console.log("Error retrieving keyframes: ", err);
+//     });
+
+
+
 		}
 	}
 });
+
+// Pulled this from local db :p
+// select id, filename, text_state from Keyframes where id = 68;
+//require('/Users/randallwong/Desktop/FoundationsPrework/gitplayback/db/models/')(sequelize);
