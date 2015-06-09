231b32c985d83ca7bb66c0e94410badb13e38a7f
darksim735 <darksim735@gmail.com>
2015-06-09 19:30:10 -0400

    Keyframe factory retrieves array of keyframes successfully

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,80 @@
 var app = angular.module('gitplayback', ['ui.router']);
 
 
-app.controller('MainController', function ($scope, KeyframeFactory) {
+var models = require('/Users/randallwong/Desktop/FoundationsPrework/gitplayback/db/models/');
+var Keyframe = models.Keyframe;
+
+app.controller('MainController', function ($scope, KeyframeFactory, nwguiFactory) {
 
 $scope.framesArray = ['nothing'];
 $scope.currentFrame = "no current frame";
 
-$scope.getall = KeyframeFactory.getKeyframe($scope.currentFrame);
 
-});
+var nwgui = nwguiFactory;
+//var folder_view = folderviewFactory;
 
-app.factory('KeyframeFactory', function () {
-	return {
-		getKeyframe: function(input){
-			// sequelize query goes here
+nwgui.Window.get().showDevTools();
 
-			// this will eventually be an array, but just setting 1 for now
-			var next = "next keyframe";
-			console.log("got keyframe?");
 
-			window.setTimeout(function(){
-			input = next;
-			console.log("set timeout");
-			}, 2000)
+$scope.getall = KeyframeFactory.getKeyframe();
 
 // Keyframe
-//     .findAll({})
+//     .findAll()
 //     .then(function() {
       
 //     })
-//     .then(function() {
-//       console.log("Fetched all keyframes succesfully"); 
+//     .then(function(keyframes) {
+//       console.log("Fetched all keyframes succesfully");
+//       console.log("keyframes:", keyframes);
 //     })
 //     .catch(function(err) {
 //       console.log("Error retrieving keyframes: ", err);
 //     });
+
+
+ });
+
+
+app.factory('nwguiFactory', function(){
+	var nwGui = require('nw.gui');
+	return nwGui;
+ });
+
+// app.factory('folderviewFactory', function(){
+// 	var folder_view = require('folder_view');
+// 	return 5;
+//  });
+
+app.factory('KeyframeFactory', function () {
+	return {
+		getKeyframe: function(){
+			// sequelize query goes here
+
+			// this will eventually be an array, but just setting 1 for now
+			// var next = "next keyframe";
+			// console.log("got keyframe?");
+
+			// window.setTimeout(function(){
+			// input = next;
+			// console.log("set timeout");
+			// console.log("")
+			// }, 2000)
+
+
+
+
+Keyframe
+    .findAll()
+    .then(function(keyframes) {
+      console.log("keyframes:", keyframes); 
+    })
+    .then(function() {
+      console.log("Fetched all keyframes succesfully");
+
+    })
+    .catch(function(err) {
+      console.log("Error retrieving keyframes: ", err);
+    });
 
 
 
