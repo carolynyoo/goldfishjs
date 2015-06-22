014d67345ac88615489f16f879ba6c7ad0a8d39e
pdedes <dedes1821@gmail.com>
2015-06-22 09:36:56 -0400

    switching branches.

--- js/ui-routes/projectbrowser/projectbrowser.js
+++ js/ui-routes/projectbrowser/projectbrowser.js
@@ -7,12 +7,20 @@ app.directive('projectbrowser', function($rootScope) {
 		restrict: 'E',
 		templateUrl: 'js/ui-routes/projectbrowser/projectbrowser.html',
 		controller: function ($scope, CommLinkFactory) {
+
 			$scope.fileTree = require(path.join(process.env.PWD, "js", "dirTree.js"));
+			$scope.currentNode = {};
+
+			$scope.$watch(function(scope) { return scope.currentNode; },
+			              function(scope) {
+			              	console.log("$watch: ", scope.currentNode);
+			              });
 
 			// On click, will broadcast via commLink to other directives that are listening.
-			$scope.selectFile = function () {
-				console.log("Directive: file selected button clicked: ");
-				CommLinkFactory.updateBrowser($scope.fileTree);
+			$scope.selectFile = function (file) {
+				console.log("Browser: this file passed in from tree: ", file);
+				console.log("Browser: this $scope.currentNode:       ", $scope.currentNode);
+				CommLinkFactory.updateBrowser(file);
 			};
 		}
 	};
