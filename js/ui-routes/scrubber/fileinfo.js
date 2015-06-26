530610950a4b3f6c71bb63c1b16ccb6dbe8a107a
darksim735 <darksim735@gmail.com>
2015-06-26 06:17:18 -0400

    Fixed toggle fileinfo panel using ng-show instead of active class

--- js/ui-routes/scrubber/fileinfo.js
+++ js/ui-routes/scrubber/fileinfo.js
@@ -4,13 +4,17 @@ app.directive('fileinfo', function($rootScope) {
 		templateUrl: 'js/ui-routes/scrubber/fileinfo.html',
 		link: function (scope, element, attr) {
 
+//		console.log("scope.showFileTab before instantiated", scope.showFileTab);
+		scope.showFileTab = true;
+//		console.log("scope.showFileTab after set true", scope.showFileTab);
+		scope.toggleActive = function() {
+//		console.log("scope.showFileTab", $scope.showFileTab);
+//		console.log("NG CLICK");
+	    
+	    scope.showFileTab = scope.showFileTab === false ? true: false;
+//	    console.log("scope.showFileTab value after click", scope.showFileTab);
+		};
 
-			// console.log("THIS AFTER TOGGLE:", this);
-			// element.bind('click', function(e){
-			// console.log(element.find('panel'));
-			// //.toggleClass("active");
-			// console.log("THIS AFTER TOGGLE:", this);
-			// });
 
 		}
 	};
