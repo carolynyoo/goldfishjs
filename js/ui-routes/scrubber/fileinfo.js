565646998a71828ed24927fbef745df8bac37518
darksim735 <darksim735@gmail.com>
2015-06-26 06:23:04 -0400

    Cleaned up comments

--- js/ui-routes/scrubber/fileinfo.js
+++ js/ui-routes/scrubber/fileinfo.js
@@ -4,17 +4,11 @@ app.directive('fileinfo', function($rootScope) {
 		templateUrl: 'js/ui-routes/scrubber/fileinfo.html',
 		link: function (scope, element, attr) {
 
-//		console.log("scope.showFileTab before instantiated", scope.showFileTab);
-		scope.showFileTab = true;
-//		console.log("scope.showFileTab after set true", scope.showFileTab);
-		scope.toggleActive = function() {
-//		console.log("scope.showFileTab", $scope.showFileTab);
-//		console.log("NG CLICK");
-	    
-	    scope.showFileTab = scope.showFileTab === false ? true: false;
-//	    console.log("scope.showFileTab value after click", scope.showFileTab);
-		};
-
+			scope.showFileTab = true;
+			scope.toggleActive = function() {
+			// Toggles true false	    
+		    scope.showFileTab = scope.showFileTab === false ? true: false;
+			};
 
 		}
 	};
