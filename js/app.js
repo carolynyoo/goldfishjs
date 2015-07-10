1271076e7fa50ce6400ad4b5fc4d9680362bf765
pdedes <dedes1821@gmail.com>
2015-07-10 17:25:11 -0400

    added a hover directive on repos in the loader view state.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,16 @@
 var fs = require('fs');
 var path = require('path');
 
-// var _ = require('lodash');
-
 // Database Paths for injecting db value into angular app
 var appPath = process.env.PWD;
 var appConfigPath = process.env.HOME + '/Library/Preferences/Saved\ Application\ State/goldfishjs';
 var dbPath = path.join(appPath, 'db/models');
 
-// console.log("%$%$%$ APPCONFIGPATH: ", appConfigPath);
-// console.log("%$%$%$ appPATH:       ", appPath);
-
 var dbFactory = require(dbPath);
 
 // Boot up the Angular App
-
 var app = angular.module('gitplayback', ['angularTreeview', 'ui.router', 'ui.ace', 'ngAnimate', 'ngAria', 'ngMaterial', 'ngMdIcons']);
 app.config(function ($urlRouterProvider, $locationProvider) {
-    // This turns off hashbang urls (/#about) and changes it to something normal (/about)
-    // $locationProvider.html5Mode(true);
-    // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
     $urlRouterProvider.otherwise('/');
 });
 
