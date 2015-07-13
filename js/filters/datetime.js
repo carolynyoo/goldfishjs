d07139476d75acc07ee32432eeac87af6cb2242a
pdedes <dedes1821@gmail.com>
2015-07-12 23:45:37 -0400

    fixed css and html to display the file info tab cleanly and in the bottom corner.

--- js/filters/datetime.js
+++ js/filters/datetime.js
@@ -1,33 +1,12 @@
-// Filter to convert datetime to human-readable
-// app.filter('datetime', function($filter) {
-// 	return function(input) {
-// 		if(input === null){ 
-// 			return ""; 
-// 		} 
-// 		input = Number(input);
-// 		var _date = $filter('date')(new Date(input),
-// 	                              'MMM dd yyyy - HH:mm:ss');
-// 		return _date;
-//  	};
-// });
-
 var moment = require('moment');
 moment().format();
 
 app.filter('myDateFormat', function myDateFormat($filter){
   return function(text){
-    // var  tempdate = new Date(text);
-    // return $filter('date')(tempdate, "MMM-dd-yyyy");
-
     var num = parseInt(text, 10);
-    console.log("PARSED TIME: ", num);
     var milli = num * 1000;
 
     var time = moment(milli);
-    var s = time.format("llll");
-
-    console.log("TIME: ", time);
-    console.log("MMNT: ", s);
-    return s;
+    return time.format("lll");
   };
 });
